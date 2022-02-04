7bc0344dd792e4a9d602ee1565449918e78654e6
Chris Lazzarini <chris.lazzarini@gmail.com>
2022-02-03 21:45:43 -0600

    added nodemon, jest, express, mongoose

--- db/db.js
+++ db/db.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/test');
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://localhost:27017/test');
+}
