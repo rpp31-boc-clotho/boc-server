4da38fa1b4490db88d8529985fd75bc3a6ea2f48
Chris Lazzarini <chris.lazzarini@gmail.com>
2022-02-05 14:56:32 -0600

    added default port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
 
 const router = require('./routes');
 
