562a5a612b6f494fe9f6d496ad38282254a5b30e
Chris Lazzarini <chris.lazzarini@gmail.com>
2022-02-15 23:42:26 -0600

    added getUser route for profile page

--- server/controllers.js
+++ server/controllers.js
@@ -1,8 +1,24 @@
+const { getUser } = require('./models');
+
 module.exports = {
   getHomePageInfo: (req, res) => {
 
   },
+
   getLoggedInInfo: (req, res) => {
+
+  },
+
+  getUserDetails: (req, res) => {
+    console.log(req.body.username);
+    let username = req.body.username;
+
+    getUser(username)
+    .then((data) => {
+      console.log('data', data);
+      res.status(200).json(data);
+    })
+    .catch((err) => {console.log(err)})
 
   }
 }
--- server/models.js
+++ server/models.js
@@ -1,11 +1,14 @@
-let { Movie } = require('../db/connection');
-console.log(Movie);
+const { Movie, User } = require('../db/connection');
+const mongoose = require('mongoose');
 
 module.exports = {
   getMovie: async (movie) => {
     const data = await Movie.find({});
     console.log(data);
-  }
-}
+  },
 
-module.exports.getMovie();
+  getUser: async (username) => {
+    return await User.find({username: username});
+  }
+
+}
