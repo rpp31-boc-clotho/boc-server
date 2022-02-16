cadfad4bbdf17dd0bd461052577531b193fc0920
Chris Turcios <christopher.d.turcios@gmail.com>
2022-02-15 19:31:33 -0800

    updated routes

--- server/models.js
+++ server/models.js
@@ -1,11 +1,67 @@
-let { Movie } = require('../db/connection');
-console.log(Movie);
+let { User, Review, Movie } = require('../db/connection');
+const {
+  getMovieAPI,
+  getMovieProvidersAPI,
+  getMovieRecommendationsAPI,
+  getPopularMoviesAPI
+} = require('./apiHelpers/movieHelpers');
+
+const setMovieRecommendations = async (movie) => {
+  const results = await getMovieRecommendationsAPI(movie.id);
+  movie.recommended = results;
+}
 
 module.exports = {
-  getMovie: async (movie) => {
-    const data = await Movie.find({});
-    console.log(data);
+  getPopularMoviesFromDB: async () => {
+    const popularMovies = await Movie.find({ popular: true });
+
+    if (popularMovies.length) {
+      return popularMovies;
+    }
+
+    // if popular movies not found in DB, retrieve from API
+    const movies = await getPopularMoviesAPI();
+
+    await Promise.all(movies.map(async (movie) => {
+      // transform movie data
+      movie.popular = true;
+      await setMovieRecommendations(movie);
+
+      let newMovie = new Movie(movie);
+
+      try {
+        await newMovie.save();
+      } catch (error) {
+        console.log(error);
+      }
+    }));
+
+    return movies;
+  },
+  getMovieFromDB: async (movie) => {
+    const movieData = await getMovieAPI(movie);
+
+    if (movieData.length) {
+      return movieData;
+    }
+
+    // if movie not found in DB, retrieve from API
+    const movieDetails = await getMovieAPI(movie);
+    await setMovieRecommendations(movie);
+
+    let newMovie = new Movie(movieDetails);
+
+    try {
+      await newMovie.save();
+    } catch (error) {
+      console.log(error);
+    }
+
   }
 }
 
-module.exports.getMovie();
+// const deleteDB = async () => {
+//   await Movie.deleteMany();
+// }
+
+// deleteDB();
