59ded6b7871a32d040285d837ad1a7c3b6a20793
Juan Acosta <jacosta091@gmail.com>
2022-02-22 21:09:26 -0700

    Took out CORS proxy, will get back to it

--- server/server.js
+++ server/server.js
@@ -8,10 +8,10 @@ const router = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
 
 app.use('/', router);
 
